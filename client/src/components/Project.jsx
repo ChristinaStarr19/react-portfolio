aafeaad241031bc0b81f381cc2daf48c75bafea8
Christina Starr <christinastarr19@gmail.com>
2020-08-07 10:41:29 -0400

    changed styling on div for project component

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -6,17 +6,17 @@ const styles = {
 
 const Project = (props) => {
     return (
-        <div>
+        <div className="col-sm-12 col-md-6 col-lg-12 col-xl-6" style={styles.figure}>
 
         
               {/* DinMo Project 1 */}
-              <figure className="col-sm-12 col-md-6 col-lg-12 col-xl-6" style={styles.figure}>
+              {/* <figure className="col-sm-12 col-md-6 col-lg-12 col-xl-6" style={styles.figure}> */}
                 <h5 className="project-title">{props.title}</h5>
                 <img className="img-fluid" src={props.image} alt={props.title}/>
                 <a  className="centered-link left-float" target="_blank" rel="noopener noreferrer" href={props.deployedlink}>Deployed Website</a>
               
                 <a className="centered-link right-float" target="_blank" rel="noopener noreferrer" href={props.githublink}>GitHub Repository</a>
-              </figure> 
+               
               </div>
     );
 };
